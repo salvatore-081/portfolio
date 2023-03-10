0f491851c0acfe636ff65ec6c5cefc8105f594fe
Salvatore <s.emilio@reply.it>
2023-03-10 12:26:33 +0100

    implemented prefersreducedmotions

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,17 @@ import logo from "../assets/logo.svg";
 import { useTranslation } from "react-i18next";
 import Select from "./Select";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
+import usePrefersReducedMotion from "../hooks/UsePrefersReducedMotion";
 
 function Header() {
   const [isMounted, setIsMounted] = useState(false);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
   const { i18n, t } = useTranslation();
   const scrollDirection = useScrollDirection();
   const scrolledDown = useScrolledDown();
 
-  const NAV_LINKS = ["about", "experience", "work", "contact"];
+  const NAV_LINKS = Object.keys(t("Header", { returnObjects: true }));
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -26,50 +29,73 @@ function Header() {
   }, []);
 
   return (
-    <div
+    <header
       className={`header ${scrollDirection === "down" ? "hide" : ""} ${
         scrolledDown && scrollDirection === "up" ? "shadow" : ""
       }`}
     >
       <img className="logo" src={logo} alt="logo"></img>
       <nav className="navigation">
-        <TransitionGroup component={null}>
-          {isMounted &&
-            NAV_LINKS.map((v, i) => (
-              <CSSTransition key={i} classNames={"fadedown"} timeout={1000}>
-                <div style={{ transitionDelay: `${i * 120}ms` }}>
-                  <a href={`#${v}`} className="navigation-link">
-                    <span className="navigation-link-index">{`0${
-                      i + 1
-                    }.`}</span>
-                    <span>{t(`Header.${v}`)}</span>
-                  </a>
-                </div>
-              </CSSTransition>
+        {prefersReducedMotion ? (
+          <>
+            {NAV_LINKS.map((v, i) => (
+              <a key={i} href={`#${v}`} className="navigation-link">
+                <span className="navigation-link-index">{`0${i + 1}.`}</span>
+                <span>{t(`Header.${v}`)}</span>
+              </a>
             ))}
-        </TransitionGroup>
+            <Select
+              value={i18n.language}
+              options={[
+                { value: "en", label: "English" },
+                { value: "it", label: "Italiano" },
+              ]}
+              onSelectChange={(e) => i18n.changeLanguage(e)}
+            />
+          </>
+        ) : (
+          <>
+            <TransitionGroup component={null}>
+              {isMounted &&
+                NAV_LINKS.map((v, i) => (
+                  <CSSTransition key={i} classNames={"fadedown"} timeout={1000}>
+                    <div style={{ transitionDelay: `${i * 120}ms` }}>
+                      <a href={`#${v}`} className="navigation-link">
+                        <span className="navigation-link-index">{`0${
+                          i + 1
+                        }.`}</span>
+                        <span>{t(`Header.${v}`)}</span>
+                      </a>
+                    </div>
+                  </CSSTransition>
+                ))}
+            </TransitionGroup>
+            <TransitionGroup component={null}>
+              {isMounted && [
+                <CSSTransition
+                  key={NAV_LINKS.length}
+                  classNames={"fadedown"}
+                  timeout={1000}
+                >
+                  <div
+                    style={{ transitionDelay: `${NAV_LINKS.length * 120}ms` }}
+                  >
+                    <Select
+                      value={i18n.language}
+                      options={[
+                        { value: "en", label: "English" },
+                        { value: "it", label: "Italiano" },
+                      ]}
+                      onSelectChange={(e) => i18n.changeLanguage(e)}
+                    />
+                  </div>
+                </CSSTransition>,
+              ]}
+            </TransitionGroup>
+          </>
+        )}
       </nav>
-      <TransitionGroup component={null}>
-        {isMounted && [
-          <CSSTransition
-            key={NAV_LINKS.length}
-            classNames={"fadedown"}
-            timeout={1000}
-          >
-            <div style={{ transitionDelay: `${NAV_LINKS.length * 120}ms` }}>
-              <Select
-                value={i18n.language}
-                options={[
-                  { value: "en", label: "English" },
-                  { value: "it", label: "Italiano" },
-                ]}
-                onSelectChange={(e) => i18n.changeLanguage(e)}
-              />
-            </div>
-          </CSSTransition>,
-        ]}
-      </TransitionGroup>
-    </div>
+    </header>
   );
 }
 
--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,10 +1,19 @@
 import "../styles/Splash.css";
 import logo from "../assets/logo.svg";
+import usePrefersReducedMotion from "../hooks/UsePrefersReducedMotion";
 
 function Splash() {
+  const prefersReducedMotion = usePrefersReducedMotion();
+
   return (
     <div className="splash">
-      <img className="splash-logo" src={logo} alt="logo"></img>
+      <img
+        className={`splash-logo ${
+          prefersReducedMotion ? "" : "splash-logo-animated"
+        }`}
+        src={logo}
+        alt="logo"
+      ></img>
     </div>
   );
 }

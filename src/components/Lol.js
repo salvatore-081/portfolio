867f7793e7ba75969488f38c8ba71448c1e04e36
Salvatore <s.emilio@reply.it>
2023-03-10 20:06:17 +0100

    lol

--- src/components/Lol.js
+++ src/components/Lol.js
@@ -1,11 +1,47 @@
 import Cat from "../assets/AggravatingQuaintBittern.webp";
+import { useState, useEffect } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import usePrefersReducedMotion from "../hooks/usePrefersReducedMotion";
 
 function Lol() {
-  return (
-    <img
-      style={{ width: "80px", position: "fixed", bottom: "0", right: "0" }}
-      src={Cat}
-    ></img>
+  const [isMounted, setIsMounted] = useState(false);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      setIsMounted(true);
+      return () => {};
+    }
+    const timeout = setTimeout(() => {
+      setIsMounted(true);
+    }, 100);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [prefersReducedMotion]);
+
+  return prefersReducedMotion ? null : (
+    <TransitionGroup component={null}>
+      {isMounted && (
+        <CSSTransition
+          key={0}
+          classNames={`${prefersReducedMotion ? "" : "fadeup"}`}
+          timeout={3000}
+        >
+          <img
+            style={{
+              transitionDelay: "2s",
+              width: "80px",
+              position: "fixed",
+              bottom: "0",
+              right: "0",
+            }}
+            src={Cat}
+          ></img>
+        </CSSTransition>
+      )}
+    </TransitionGroup>
   );
 }
 
